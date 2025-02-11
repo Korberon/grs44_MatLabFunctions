5e12a8f09eb01285500e56c3822c30a1b3603a0f
George Smith / "Greg <138257731+Korberon@users.noreply.github.com>
2025-02-11 15:59:29 +0000

    New startup.m
    Auto find the genpath of the other functions - change the addPath directory to yours instead of mine!
    Made it so the dark-mode graphics theme is always light for figures (because why would you use dark mode figures?!)

--- startup.m
+++ startup.m
@@ -1,8 +1,12 @@
 userProfile = getenv('USERPROFILE');
 addpath(genpath(fullfile(userProfile, 'OneDrive - University of Bath', '(_PhD', 'MatLab Functions')));
+clear userProfile
 
 set(groot, 'DefaultLineLineWidth',2) ;
 set(groot, 'DefaultTextInterpreter','latex');
 set(groot, 'DefaultAxesTickLabelInterpreter','latex');
 set(groot, 'DefaultLegendInterpreter','latex');
 set(groot, 'DefaultAxesFontSize',18);
+
+s = settings;
+s.matlab.appearance.figure.GraphicsTheme.TemporaryValue = "light";
