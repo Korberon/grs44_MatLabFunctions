bc0ab712d73e71949e5a616a36be2fdc288e00eb
George Smith / "Greg <138257731+Korberon@users.noreply.github.com>
2025-02-11 15:55:23 +0000

    ReadMe of annoTableRaw.m
    Minor updates to annoTableRaw.m and improved the "help annoTableRaw"

--- annoTableRaw.m
+++ annoTableRaw.m
@@ -1,4 +1,13 @@
 function an = annoTableRaw(ax,position)
+%% annoTableRaw.m
+%   Creates an Annotation Table (see annoTable.m), but does it in a very low-level way
+%% Inputs       :
+%   ax          : Axes object
+%   position    : Position
+%% Outputs      :
+%   an          : Annotation table object
+%
+%% Created by George R. Smith - grs44@bath.ac.uk 
 
 ax.Units = 'pixels' ; 
 an = annotation('textbox','Units','pixels','Position',position.*ax.Position) ; 
