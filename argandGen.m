7b242c59ad5287891e5f7e16ffefdf7d9602f4f2
George Smith / "Greg <138257731+Korberon@users.noreply.github.com>
2025-02-11 14:38:14 +0000

    Updated argandGen()
    To work with figGen() a bit easier

--- argandGen.m
+++ argandGen.m
@@ -1,8 +1,20 @@
 function [fg,ax,lg] = argandGen(varargin)
-lg = [] ; 
-if contains(varargin,"lg") , [fg,ax,lg] = figGen('square',varargin) ; else , [fg,ax] = figGen('square') ; end 
+%% argandGen.m
+% Very simple couple of lines of code to just set up an Argand diagram
+%% Inputs   :
+% None
+%% Outputs  :
+% fg        : Figure object
+% ax        : Axes object
+% lg        : Legend object
+%% Optional Inputs  :
+% See figGen for details
+%% Example Code :
+% argandGen() ; 
+% argandGen('lg',true) ; 
+% [fg,ax] = argandGen('Title',true) ; ax.Position(1) = 0.2 ; title("Hysteresis") ; tList = 0:2^-13:20 ; plot(cos(tList*2*pi*15+0.1)*0.5,sin(tList*2*pi*15+1.31)*0.5)
+
+[fg,ax,lg] = figGen('Square',true,varargin{:}) ;
 xlabel("Re") ; ylabel("Im") ; 
 xline(0,'LineWidth',1,'Color','k','Alpha',1,'HandleVisibility','off') ; yline(0,'LineWidth',1,'Color','k','Alpha',1,'HandleVisibility','off') ; 
-% plot(0.001,0.001) ; 
-% plot(-0.001,-0.001) ; 
 axis equal 
