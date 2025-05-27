ce5d4a59511d6f482d6d64205f30ba4adefe2623
George Smith / "Greg <138257731+Korberon@users.noreply.github.com>
2025-05-27 15:35:16 +0100

    Instructions Added
    Added instructions / comments for startup.m for clearer use

--- startup.m
+++ startup.m
@@ -1,3 +1,11 @@
+%% startup.m
+% File that runs on startup / when cleared with sodoff.m
+%% Inputs   :
+%   Will request a path on first run; select the directory where you store your MatLab Functions
+%% Outputs  :
+%   Will create "myUserProfile.mat" which saves the directory selected by the UI Request
+
+%% Request and Save Generic Path
 userProfile = getenv('USERPROFILE');
 startPath = mfilename('fullpath') ; startPath = startPath(1:length(startPath)-length(mfilename)) ; 
 if ~exist('myUserProfile.mat','file')
@@ -6,19 +14,23 @@ if ~exist('myUserProfile.mat','file')
     loc = loc(length(userProfile)+1:end) ; 
     save([startPath,'myUserProfile.mat'],'loc') ; 
 end
+
+%% Load Path
 load([startPath,'myUserProfile.mat'],'loc') ; 
 addpath(genpath(fullfile(userProfile,loc))) ; 
-warning('off','MATLAB:MKDIR:DirectoryExists') ; 
 
+%% Graphical Root Settings
 set(groot, 'DefaultLineLineWidth',2) ;
 set(groot, 'DefaultTextInterpreter','latex');
 set(groot, 'DefaultAxesTickLabelInterpreter','latex');
 set(groot, 'DefaultLegendInterpreter','latex');
 set(groot, 'DefaultAxesFontSize',18);
 
+%% Use Light Setting if using Dark-Mode Shell in 2023b Onwards
 s = settings;
 s.matlab.appearance.figure.GraphicsTheme.TemporaryValue = "light";
 clear all ; 
 
-%% Disable Warnings
+%% Disable Annoying Warnings
+warning('off','MATLAB:MKDIR:DirectoryExists') ; 
 warning('off','stats:boxplot:BadObjectType') ; 
